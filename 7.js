a191ec536f9f6bd8d934fcc46e19b2c1d7438ef5
Zia ur rehman <119785513+Ziaengr@users.noreply.github.com>
2023-09-08 09:05:53 -0700

    Add files via upload

---
+++ 7.js
@@ -0,0 +1,182 @@
+/*Q:1:
+var num1= 3;
+var num2= 5;
+var  sum = num1 + num2;
+document.write("sum  of" +   num1 +" and" + num2 + "is" + sum);*/
+
+/*Q:2(a):
+var num1 = 3;
+var num2 = 5;
+var subtract = num1 - num2;
+document.write("subtract of" + num1 +"and" + num2 +"is" + subtract);*/
+
+/*Q:2(b):
+var num1 = 3;
+var num2 = 5;
+var multiply = num1 * num2;
+document.write(" multiply of" + num1 +"and" + num2 +"is" +  multiply);*/
+
+/*Q:2(c):
+var num1 = 3;
+var num2 = 5;
+var divide = num1 / num2;
+document.write(" divide of" + num1 +"and" + num2 +"is" +   divide);*/
+
+/*Q:2(d):
+var num1 = 3;
+var num2 = 5;
+var modulus = num1 % num2;
+document.write(" modulus of" + num1 +"and" + num2 +"is" +   modulus);*/
+
+/*Q:3(a&b):
+var value ;
+document.write('value after variable declaration is'+value);*/
+
+/*Q:3(c and d):
+var value = 5;
+document.write("Initial value"+" "+value);*/
+
+/*Q:3(e):
+var x= 5;
+var y= ++x;
+document.write("value after increment is"+" " + value);*/
+
+/*Q:no:(f):
+var x=5;
+var y=++x;
+var z= y +7;
+document.write("value after addition"+" "+z)*/
+
+/*Q:3(g&h):
+var x = 5;
+var y = ++x;
+var z = y + 7;
+document.write("value after addition"+" "+z)*/
+
+/*Q:3(i&j):
+var x=5
+var y=++y;
+var z=y+7;
+var p=--z;
+document.write("value after decremnt"+" "+p)*?
+
+/*Q:(k&l):
+var x=5;
+var y=++x;
+var z=y+7;
+var p=--z;
+var q=p%3;
+document.write("The remainder is"+" "+q)*/
+
+/*Q:4:
+var ticket= 600;
+var fiveTicket= ticket * 5;
+var result = fiveTicket;
+document.write('Total cost to buy 5 tickets to a movie is'+ result +"PKR")*/
+
+/*Q:no:5
+var x = 4;
+ for (i=1 ; i<=10; i++){
+     let y = `${x} X ${i} = ${x*i} <br/>`
+   document.write(y)
+  }*/
+
+  /*Q NO: 6( a & b ): 
+  var C= 25;
+  var cTof= (25*9/5)+32
+ document.write( C + '&deg;C'+ "  " + "is" + " "+cTof +'F&deg;') ;*/
+
+/*Q NO :(c & d): 
+var F = 70;
+var fToc= (70-32)*5/9;
+ document.write( F + 'F&deg;' +  " " + "is"+" "+ fToc +'&deg;C');*/
+
+/*Q NO :7:
+ var priceItemOne = 650;
+  document.write("Price of item 1 is "+priceItemOne +"<br/>") ;
+
+ /*var quantityItemOne = 3;
+document.write("Quantity of item 1 is "+quantityItemOne + "<br/>" )
+
+  var priceItemTwo = 100;
+ document.write("Price of item 2 is "+priceItemTwo +"<br/>") ;
+ var quantityItemTwo = 7;
+document.write("Quantity of item 2 is "+quantityItemTwo + "<br/>" )
+ var shoppingCharge = 100;
+  document.write("Shopping charges "+shoppingCharge + "<br/>" )
+
+  var total = priceItemOne * quantityItemOne + priceItemTwo * quantityItemTwo + shoppingCharge;
+ document.write("Total cost of your order is "+total)*?
+
+
+
+
+
+/* Q NO 8: 
+
+ var totalMarks = 980;
+ document.write("Total marks :" + totalMarks +"<br/>")
+ var obtMarks = 804;
+ document.write("Marks obtained : " + obtMarks +"<br/>")
+ var percentage = 804/980*100;
+ document.write("Percentage :" + percentage)*/
+
+
+ /*Q NO 9: 
+
+ var usDoller =10;
+ var riyadcur = 25;
+ var inPKR = usDoller * 307.79 + riyadcur * 82.05305;
+ document.write("Total Currency in PKR :" + inPKR)*/
+
+/*Q NO 10:
+
+ var x = 5;
+ var y = x+ 5*10/2;
+ document.write('Result is ' + y)*/
+
+/*Q NO 11: 
+
+ var current = 2016;
+ var birth = 1992;
+ var age = 2016-1992;
+ document.write("Your Age is:"+ age);*/
+
+/* Q NO 12:
+
+  var radius = 20;
+  document.write("Radius of a circle:" + radius +"<br/>");
+ var circum = 2 *3.142*20;
+ document.write("The circumference is :" + circum +"<br/>");
+ var aera = 3.142*20*20;
+ document.write("The aera is :"+aera);*/
+
+/*Q NO 13 :
+var snack = "chocolate chip ";
+var currentAge = 19;
+var EstMaxAge = 65;
+var AmountSnack = 3;
+var totalSnack = 41*12*30 *3;
+document.write(`You will need ${totalSnack} chocolate chip to last you until the ripe old age of  ${EstMaxAge} `) */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
